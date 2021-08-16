5ffae815b046ce3d5aa8c1ae174b9579bacf9465
Leonard <s_xsipo6@uni-bremen.de>
2021-08-16 20:30:52 +0200

    add ability to fetch polygon ethereum balance

--- Python/Ethermine/ethermine_fetch_outstanding_balance.py
+++ Python/Ethermine/ethermine_fetch_outstanding_balance.py
@@ -1,4 +1,3 @@
-# Note: fetching the payout date is momentarily not possible
 from requests_html import HTMLSession
 import requests
 import json
@@ -20,7 +19,13 @@ HASHRATE_DIGITS = 2
 ADDRESS = "YOUR_ETH_ADDRESS"
 
 # Your Etherscan API Token
-API_TOKEN = 'YOUR_ETHERSCAN_API_KEY'
+API_TOKEN = 'YOUR_ETHERSCAN_API_TOKEN'
+
+# Your Polyscan API Token
+POLY_API_TOKEN = 'YOUR_POLYSCAN_API_TOKEN'
+
+# Wrapped ethereum contract id
+WRAPPED_ETHER = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
 
 # Cache of javascript websites
 ethermine_cache = { }
@@ -29,19 +34,20 @@ ethermine_cache = { }
 ethermine_api_cache = { }
 
 
+"""
 # Loads Javascript content in webpages and then caches them
 def get_cached_website(url):
 	if url in ethermine_cache:
 		return ethermine_cache[url]
 	else:
 		session = HTMLSession()
-	
 		r = session.get(url)
-		r.html.render(timeout = 0, sleep = 1)
+		r.html.render(timeout = 4, sleep = 4)
 
 		ethermine_cache[url] = r.html.raw_html
 
 		return ethermine_cache[url]
+"""
 
 
 # Fetches ethermine api data
@@ -66,24 +72,27 @@ def fetch_unpaid(address):
 	return round(converted, ETH_DIGITS)
 
 
-# Momentarily not working
+"""
 def fetch_payout_date(address):
 
 	url = f'https://ethermine.org/miners/{address}/payouts'
 	
 	soup = BeautifulSoup(get_cached_website(url), "html.parser")
-	elements = soup.find_all("div", class_="workers card")
-
+	elements = soup.find_all("div")
+	print(soup)
 	for element in elements:
-		if 'Next Payout' in str(element):
+		print(element)
+		if 'days' in str(element):
 			break
 
 	try:
+		print(element)
 		next_payout = str(element).split('≈')[1].split('>')[1].split('<')[0]
 	except:
 		next_payout = "N/A"
 		
 	return next_payout
+"""
 
 
 def fetch_current_hashrate(address):
@@ -152,13 +161,46 @@ def fetch_ether_balance(address, api_key):
 
 	response_content = json.loads(r.text)
 
-	# result needs to be divided by 10^18 to gain actual eth balance
 	return round(float(response_content.get('result')) / ETH_NORMAL, ETH_DIGITS)
 
 
-if __name__ == '__main__':
-	print('===== Ethermine status =====\n')
-	print(f'Account Balance: {str(fetch_ether_balance(ADDRESS, API_TOKEN))} ⧫\n')
+def fetch_poly_balance(address, api_key, wrapped_eth):
+	url = f'https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress={wrapped_eth}&address=0x{address}&tag=latest&apikey={api_key}'
+
+	r = requests.get(url)
+
+	response_content = json.loads(r.text)
+
+	return round(float(response_content.get('result')) / ETH_NORMAL, ETH_DIGITS)
+
+
+def get_ether_price(api_key):
+	url = f'https://api.etherscan.io/api?module=stats&action=ethprice&apikey={api_key}'
+
+	r = requests.get(url)
+
+	response_content = json.loads(r.text)
+
+	return response_content.get('result').get('ethusd')
+
+
+def get_account_statistics():
+	global ADDRESS, API_TOKEN, POLY_API_TOKEN, WRAPPED_ETHER
+	eth_balance = fetch_ether_balance(ADDRESS, API_TOKEN)
+	poly_balance = fetch_poly_balance(ADDRESS, POLY_API_TOKEN, WRAPPED_ETHER)
+	total_balance = eth_balance + poly_balance
+	balance_USD = round(float(get_ether_price(API_TOKEN)) * total_balance, 2)
+	print('--- Account Statistics ---')
+	print(f'Account Balance: {str(eth_balance)} ⧫')
+	print(f'Polygon Balance: {str(poly_balance)} ⧫')
+	print(len(str(poly_balance)) * '-' + '--------------------')
+	print(f'Total Balance: {str(total_balance)} ⧫')
+	print(f'Total Balance USD: {str(balance_USD)} $\n')
+
+
+def get_miner_statistics():
+	global ADDRESS
+	print('--- Miner Statistics ---')
 	print(f'Current Hashrate: {fetch_current_hashrate(ADDRESS)} MH/s')
 	print(f'Reported Hashrate: {fetch_reported_hashrate(ADDRESS)} MH/s')
 	print(f'Unpaid Balance: {fetch_unpaid(ADDRESS)} ⧫')
@@ -167,4 +209,10 @@ if __name__ == '__main__':
 	print(f'Invalid Shares: {str(fetch_invalid_shares(ADDRESS))}')
 	print(f'Stale Shares: {str(fetch_stale_shares(ADDRESS))}\n')
 	#print(f'Next Payout in: {fetch_payout_date(ADDRESS)} days\n')
+
+
+if __name__ == '__main__':
+	print('===== Ethermine status =====\n')
+	get_account_statistics()
+	get_miner_statistics()
 	print('============================')
