f1db2d87b25f186ce5ffd993f7b6e874ff9169e1
Leonard <45939246+leolion3@users.noreply.github.com>
2022-03-08 19:30:16 +0100

    add spammer

---
+++ Python/ChatSpammer/spam.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+import keyboard
+import sys
+import os
+
+spam = ''
+for a in sys.argv:
+	if not (os.path.basename(__file__) in a): 
+		spam += a + ' '
+
+print('[BOOT] Starting spammer in 3 seconds...')
+print('[INFO] Press "q" anytime to quit')
+time.sleep(3)
+print('[INFO] Spamming...')
+
+while(True):
+	pyautogui.write(spam)
+	pyautogui.press('enter')
+	try:
+		if keyboard.is_pressed('q'):
+			break
+	except:
+		continue
+
+print('[EXIT] Exiting...')
