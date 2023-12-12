f79ffa26af447527b84fd4821e16a2df263b4fe7
Leonard <45939246+leolion3@users.noreply.github.com>
2023-12-12 15:20:42 +0100

    Update app.py

--- Python/iCalGenerator/app.py
+++ Python/iCalGenerator/app.py
@@ -3,6 +3,7 @@ from icalendar import Calendar, Event
 from datetime import datetime
 import re
 from unidecode import unidecode
+import json
 
 
 app = Flask(__name__)
@@ -18,27 +19,30 @@ def allowed_file(filename):
 def index():
     if request.method == 'POST':
         event_data = {
-            'summary': request.form['summary'],
-            'location': request.form['location'],
-            'description': request.form['description'],
-            'start_datetime': request.form['start_datetime'],
-            'end_datetime': request.form['end_datetime'],
+            'summary': request.form['summary'] if 'summary' in request.form else '',
+            'location': request.form['location'] if 'location' in request.form else '',
+            'description': request.form['description'] if 'description' in request.form else '',
+            'start_datetime': request.form['start_datetime'] if 'start_datetime' in request.form else '',
+            'end_datetime': request.form['end_datetime'] if 'end_datetime' in request.form else '',
             'is_meeting': 'Yes' if 'is_meeting' in request.form else 'No',
-            'meeting_url': request.form['meeting_url']
+            'meeting_url': request.form['meeting_url'] if 'meeting_url' in request.form else ''
         }
 
-        image = request.files['image']
+        image = request.files['image'] if 'image' in request.files else None
         if image and allowed_file(image.filename):
             image_path = f"uploads/{image.filename}"
             image.save(image_path)
             event_data['image'] = image_path
-
-        ical_data = generate_ical(event_data)
-        filename = f"{unidecode(event_data['summary'])}.ics"
-        filename = re.sub(r'[\/:*?"<>|]', '_', filename)
-        response = Response(ical_data, mimetype='text/calendar; charset=utf-8')
-        response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
-        return response
+        try:
+            ical_data = generate_ical(event_data)
+            filename = f"{unidecode(event_data['summary'])}.ics"
+            filename = re.sub(r'[\/:*?"<>|]', '_', filename)
+            response = Response(ical_data, mimetype='text/calendar; charset=utf-8')
+            response.headers['Content-Disposition'] = f'attachment; filename="{filename}"'
+            return response
+        else:
+            err = {'Error': 'Invalid request format!'}
+            return json.dumps(err), 400, {'content-type': 'application/json'}
 
     return render_template('index.html')
 
