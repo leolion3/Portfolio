6042d31da3e42cd40de8b66fb901b7d4ff253fb9
Leonard <45939246+leolion3@users.noreply.github.com>
2024-01-20 00:21:11 +0100

    Update install.sh

--- Linux/CustomCommands/install.sh
+++ Linux/CustomCommands/install.sh
@@ -8,17 +8,35 @@ echo '\033[0;33m=============================================\033[1;32m'
 echo ''
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 cd ~
+
+# Download components
 mkdir -p commands
 cd commands
 echo '[INFO] Downloading executables...'
 curl -LkSs https://api.github.com/repos/leolion3/Portfolio/tarball -o master.tar.gz
 tar -xzf master.tar.gz --strip-components=1
 cd ..
+
+# Move to corrent location
 cmd_dir='.commands'
 echo "[INFO] Creating executables path $cmd_dir..."
 mkdir -p "$cmd_dir"
 mv commands/Linux/CustomCommands/* "./$cmd_dir/"
+mv commands/Python/FileSender/ptransfer "./$cmd_dir/"
+mv commands/Python/PasswordUtils/passtransfer "./$cmd_dir/"
+mv commands/Python/KeepBusy/keepbusy* "./$cmd_dir/"
+mv commands/Python/PasswordVault/passwordVault.py "./$cmd_dir/pv"
+
+# Cleanup
 rm -rf commands/
+
+# Ready components
 cd "$cmd_dir/"
 rm "README.md"
+chmod +x ./ptransfer
+chmod +x ./passtransfer
+chmod +x ./keepbusy
+chmod +x ./keepbusy_cross
+chmod +x ./pv
+
 echo "Please add this to your terminal's rc file: export PATH=\"$PWD:\$PATH\""
